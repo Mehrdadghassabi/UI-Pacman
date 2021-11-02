305580a8b6e91cef1eb713ea7f58ac31f72fd395
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2021-11-02 17:06:02 +0330

    Add files via upload

--- Aigame/python_client/client_main.py
+++ Aigame/python_client/client_main.py
@@ -1,11 +1,11 @@
 import random
-import string
-
 from base import BaseAgent, Action
 from queue import PriorityQueue
 
 i = 0
 aclis = []
+available_goals = []
+unavailable_goals = []
 
 
 def manhattan(start, goal):
@@ -18,63 +18,63 @@ class Agent(BaseAgent):
     def neighbors(self, node):
         width = self.grid_width
         height = self.grid_height
-        i = node[0]
+        x = node[0]
         j = node[1]
 
         neis = []
-        if i == 0 and j == 0:
+        if x == 0 and j == 0:
             nei1 = (1, 0)
             nei2 = (0, 1)
             neis.append(nei1)
             neis.append(nei2)
-        elif i == 0 and j == width - 1:
+        elif x == 0 and j == width - 1:
             nei1 = (1, width - 1)
             nei2 = (0, width - 2)
             neis.append(nei1)
             neis.append(nei2)
-        elif i == height - 1 and j == 0:
+        elif x == height - 1 and j == 0:
             nei1 = (height - 2, 0)
             nei2 = (height - 1, 1)
             neis.append(nei1)
             neis.append(nei2)
-        elif i == height - 1 and j == width - 1:
+        elif x == height - 1 and j == width - 1:
             nei1 = (height - 1, width - 2)
             nei2 = (height - 2, width - 1)
             neis.append(nei1)
             neis.append(nei2)
-        elif i == 0 and j > 0:
+        elif x == 0 and j > 0:
             nei1 = (1, j)
             nei2 = (0, j - 1)
             nei3 = (0, j + 1)
             neis.append(nei1)
             neis.append(nei2)
             neis.append(nei3)
-        elif i > 0 and j == 0:
-            nei1 = (i, 1)
-            nei2 = (i - 1, 0)
-            nei3 = (i + 1, 0)
+        elif x > 0 and j == 0:
+            nei1 = (x, 1)
+            nei2 = (x - 1, 0)
+            nei3 = (x + 1, 0)
             neis.append(nei1)
             neis.append(nei2)
             neis.append(nei3)
-        elif i == height - 1 and j > 0:
+        elif x == height - 1 and j > 0:
             nei1 = (height - 2, j)
             nei2 = (height - 1, j - 1)
             nei3 = (height - 1, j + 1)
             neis.append(nei1)
             neis.append(nei2)
             neis.append(nei3)
-        elif i > 0 and j == width - 1:
-            nei1 = (i, width - 2)
-            nei2 = (i - 1, width - 1)
-            nei3 = (i + 1, width - 1)
+        elif x > 0 and j == width - 1:
+            nei1 = (x, width - 2)
+            nei2 = (x - 1, width - 1)
+            nei3 = (x + 1, width - 1)
             neis.append(nei1)
             neis.append(nei2)
             neis.append(nei3)
         else:
-            nei1 = (i, j + 1)
-            nei2 = (i, j - 1)
-            nei3 = (i + 1, j)
-            nei4 = (i - 1, j)
+            nei1 = (x, j + 1)
+            nei2 = (x, j - 1)
+            nei3 = (x + 1, j)
+            nei4 = (x - 1, j)
             neis.append(nei1)
             neis.append(nei2)
             neis.append(nei3)
@@ -93,7 +93,7 @@ class Agent(BaseAgent):
 
         while not frontier.empty():
             current = frontier.get()
-            # print(str(current[1][0]) + str(current[1][1]))
+            print(str(current[1][0]) + str(current[1][1]))
             if current[1][0] == goal[0] and current[1][1] == goal[1]:
                 print("Goal found")
                 break
@@ -103,42 +103,45 @@ class Agent(BaseAgent):
                 if nnext not in cost_so_far or new_cost < cost_so_far[nnext]:
                     cost_so_far[nnext] = new_cost
                     priority = new_cost + manhattan(nnext, goal)
-                    if self.grid[nnext[0]][nnext[1]] == "E":
+                    # print("ummm re" + str(self.grid[nnext[0]][nnext[1]]))
+                    if "E" in self.grid[nnext[0]][nnext[1]] or "2" in self.grid[nnext[0]][nnext[1]] \
+                            or "3" in self.grid[nnext[0]][nnext[1]] or "4" in self.grid[nnext[0]][nnext[1]]:
                         frontier.put((priority, nnext))
                         # print("nnext: " + str(nnext[0]) + str(nnext[1]) + " priority: " + str(priority))
                     came_from[nnext] = current[1]
-        # print(str(cost_so_far[(4, 4)]))
-        # print("baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         return cost_so_far, came_from
 
-    def goals_list(self):
-        available_goals = []
-        unavailable_goals = []
+    def goals_list(self, current):
+        lavailable_goals = []
+        lunavailable_goals = []
         for x in range(self.grid_height):
             for j in range(self.grid_width):
                 # print(self.grid[i][j])
                 if self.grid[x][j] == "1":
                     tup1 = (x, j)
-                    available_goals.append(tup1)
+                    lavailable_goals.append(tup1)
                 if self.grid[x][j] == "2":
                     tup1 = (x, j)
-                    if self.agent_scores[0] >= 15:
-                        available_goals.append(tup1)
+                    if self.agent_scores[0] >= 15 + manhattan(current, tup1):
+                        # print("Scpo2 : " + str(15 + manhattan(current, tup1)))
+                        lavailable_goals.append(tup1)
                     else:
-                        unavailable_goals.append(tup1)
+                        lunavailable_goals.append(tup1)
                 if self.grid[x][j] == "3":
                     tup1 = (x, j)
-                    if self.agent_scores[0] >= 50:
-                        available_goals.append(tup1)
+                    if self.agent_scores[0] >= 50 + manhattan(current, tup1):
+                        # print("Scpo3 : " + str(50 + manhattan(current, tup1)))
+                        lavailable_goals.append(tup1)
                     else:
-                        unavailable_goals.append(tup1)
+                        lunavailable_goals.append(tup1)
                 if self.grid[x][j] == "4":
                     tup1 = (x, j)
-                    if self.agent_scores[0] >= 140:
-                        available_goals.append(tup1)
+                    if self.agent_scores[0] >= 140 + manhattan(current, tup1):
+                        # print("Scpo4 : " + str(140 + manhattan(current, tup1)))
+                        lavailable_goals.append(tup1)
                     else:
-                        unavailable_goals.append(tup1)
-        return available_goals, unavailable_goals
+                        lunavailable_goals.append(tup1)
+        return lavailable_goals, lunavailable_goals
 
     def find_state(self, st):
         for x in range(self.grid_height):
@@ -151,6 +154,11 @@ class Agent(BaseAgent):
         temp_goal = goal
         list_of_action = []
         list_of_action_reversed = []
+        # print("cccc: " + str(came_from[temp_goal]))
+        while temp_goal not in came_from:
+            available_goals.remove(temp_goal)
+            unavailable_goals.append(temp_goal)
+            temp_goal = available_goals[0]
         while temp_goal != start:
             goal_nei = came_from[temp_goal]
             print("hey im in the loop temp goal is: " + str(temp_goal))
@@ -161,9 +169,9 @@ class Agent(BaseAgent):
             print("y = " + str(y))
             print("---------------------------------------------------")
             if x == 1 and y == 0:
-                list_of_action.append(Action.UP)
-            if x == -1 and y == 0:
                 list_of_action.append(Action.DOWN)
+            if x == -1 and y == 0:
+                list_of_action.append(Action.UP)
             if x == 0 and y == 1:
                 list_of_action.append(Action.RIGHT)
             if x == 0 and y == -1:
@@ -176,19 +184,59 @@ class Agent(BaseAgent):
             list_of_action_reversed.append(item)
         return list_of_action_reversed
 
+    def sort_available_goals(self, size, current):
+        print(size)
+        for x in range(size):
+            small = x
+            for j in range(x, size):
+                # print("hi: " + str(self.goal_score_function(available_goals[j], current)))
+                # print("bye: " + str(self.goal_score_function(available_goals[small], current)))
+                if self.goal_score_function(available_goals[j], current) < self.goal_score_function(
+                        available_goals[small], current):
+                    small = j
+            temp = available_goals[small]
+            available_goals[small] = available_goals[x]
+            available_goals[x] = temp
+
+        return available_goals
+
+    def goal_score_function(self, goal, current):
+        x = goal[0]
+        y = goal[1]
+        distance = manhattan(current, goal)
+        if self.grid[x][y] == "1":
+            return 10 / distance ** 2
+        elif self.grid[x][y] == "2" and self.agent_scores[0] > 15:
+            return 25 / distance ** 2
+        elif self.grid[x][y] == "3" and self.agent_scores[0] > 50:
+            return 35 / distance ** 2
+        elif self.grid[x][y] == "4" and self.agent_scores[0] > 140:
+            return 75 / distance ** 2
+        else:
+            return 0
+
     def do_turn(self) -> Action:
         global i
         global aclis
-        available_goals, unavailable_goals = self.goals_list()
+        global available_goals
+        global unavailable_goals
+        print(
+            "-----------------------------------------------------------------------------------------------------------------------------")
+        print("my score is:: " + str(self.agent_scores[0]))
+        print("turn: " + str(i))
+        start = self.find_state("A")
+        available_goals, unavailable_goals = self.goals_list(start)
+        goal = available_goals[0]
+        cost_so_far, came_from = self.cost(start, goal)
         print("av: " + str(available_goals))
         print("un: " + str(unavailable_goals))
         # print("step: " + str(i) + "score: " + str(self.agent_scores[0]))
-        start = self.find_state("A")
-        goal = available_goals[0]
-        cost_so_far, came_from = self.cost(start, goal)
-        # print("cost_so_far: " + str(cost_so_far))
+        print("my current position: " + str(start))
+        # print("distance to goal: " + str(cost_so_far[goal]))
         if not bool(aclis):
             aclis = self.eat_the_goal(start, goal, came_from)
+            available_goals = self.sort_available_goals(len(available_goals), start)
+            # aclis = self.sort_available_goals(available_goals, len(available_goals), start)
             print("the path is=== " + str(aclis))
         # print("goal came_from: " + str(came_from[goal]))
         # print(self.agent_scores)
@@ -196,6 +244,7 @@ class Agent(BaseAgent):
         # while bool(aclis):
         if bool(aclis):
             ki = aclis[0]
+            aclis.remove(ki)
             print(ki)
             return ki
         else:
